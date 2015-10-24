84e95c8f18a297d94555ea88315ba2a663e0a49c
jthidalgojr <jthidalgojr@gmail.com>
2015-10-24 14:27:49 -0700

    soda query wrapper class

---
+++ api/soql.py
@@ -0,0 +1,62 @@
+__author__ = 'James'
+
+import urllib.parse
+import http.client
+
+class SoQL:
+    def __init__(self, resourceId):
+        self.resourceId = resourceId
+        self.params = {}
+        #max limit by default
+        self.limit(50000)
+
+    def _buildRequest(self):
+        return"/resource/" + self.resourceId + ".json?" + urllib.parse.urlencode(self.params)
+
+    #returns json string from query
+    def execute(self):
+        conn = http.client.HTTPSConnection('greengov.data.ca.gov')
+        conn.request("GET", self._buildRequest(), headers={"X-App-Token": "eZ54Yp2ubYQAEO2IvzxR7pPQu"})
+        return conn.getresponse().read().decode("utf-8")
+
+    def filter(self, column, value):
+        self.params[column] = value
+        return self
+
+    def multiFilter(self, filters):
+        for k, v in filters.items():
+            self.filter(k, v)
+        return self
+
+    def select(self, columns):
+        self.params["$select"] = ",".join(columns)
+        return self
+
+    def where(self, condition):
+        self.params["$where"] = condition
+        return self
+
+    #e.g. {"total_miles": "DESC"}
+    def orderBy(self, columns):
+        columnsFormatted = [k+" "+v for k, v in columns.items()]
+        self.params["$order"] = ",".join(columnsFormatted)
+        return self
+
+    def limit(self, lim):
+        self.params["$limit"] = str(lim)
+        return self
+
+    def groupBy(self, columns):
+        self.params["$group"] = ",".join(columns)
+
+
+def test():
+    query = (
+        SoQL("aazw-6wcw")
+        .filter("disposed","No")
+        .multiFilter({"fuel_type": "EVC"})
+        .orderBy({"total_miles": "DESC"})
+        .select(["vin", "agency"])
+    )
+    print(query._buildRequest())
+    print(query.execute())
